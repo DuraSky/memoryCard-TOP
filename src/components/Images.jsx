4432ed894c23bc8ad361150d6888b257626cdcc0
Lukas Broskovic <lukas.broskovic@gmail.com>
2024-03-19 11:42:20 +0100

    add basic pokemonAPI fetching and getting names on img clikc

--- src/components/Images.jsx
+++ src/components/Images.jsx
@@ -1,6 +1,52 @@
-import React from "react";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export default function Images() {
-  return <h1>Hi</h1>;
+  const [pokemonImages, setPokemonImages] = useState({});
+
+  const getName = (name) => {
+    return console.log(name.name);
+  };
+
+  useEffect(() => {
+    const pokemonNames = [
+      "charizard",
+      "pikachu",
+      "bulbasaur",
+      "raichu",
+      "squirtle",
+      "eevee",
+    ];
+
+    const fetchPokemonImages = async () => {
+      const images = {};
+      for (const name of pokemonNames) {
+        try {
+          const response = await fetch(
+            `https://pokeapi.co/api/v2/pokemon/${name}`,
+          );
+          const data = await response.json();
+          images[name] = data.sprites.front_default;
+        } catch (error) {
+          console.error(`Error fetching ${name} image:`, error);
+        }
+      }
+      setPokemonImages(images);
+    };
+
+    fetchPokemonImages();
+  }, []);
+
+  return (
+    <div>
+      {Object.keys(pokemonImages).map((name) => (
+        <img
+          key={name}
+          src={pokemonImages[name]}
+          alt={name}
+          width="150px"
+          onClick={() => getName({ name })}
+        />
+      ))}
+    </div>
+  );
 }
